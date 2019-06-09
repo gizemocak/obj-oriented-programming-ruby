bfd8b526ca087da2768f7ed02b89d4b0515440ba
gizemocak <gizem_ocak@outlook.com>
2019-06-09 21:02:54 +0000

    accessor methods

--- 02_classes_and_objects.rb
+++ 02_classes_and_objects.rb
@@ -2,6 +2,14 @@ class GoodDog
   def initialize(name)
     @name = name #@name is an instance variable
   end
+  
+  def get_name
+    @name
+  end  
+
+  def set_name=(name)
+    @name = name 
+  end
 
   def speak
     "#{@name} says arf!"
@@ -13,6 +21,13 @@ puts sparky.speak
 
 fido = GoodDog.new("Fido")
 puts fido.speak
+
+#puts sparky.name would return a NoMethodError , we would have to create a method that will return the name
+puts sparky.get_name
+sparky.set_name = "Sparkly"
+puts sparky.get_name
+
+
 
 
 
