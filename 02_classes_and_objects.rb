a3a540a2f80ecfb737b0f1feba88d951a4189fbf
gizemocak <gizem_ocak@outlook.com>
2019-06-09 22:32:36 +0000

    create a new class and methods

--- 02_classes_and_objects.rb
+++ 02_classes_and_objects.rb
@@ -1,3 +1,4 @@
+#-----------GoodDog Class-----------#
 class GoodDog
   attr_accessor :name, :height, :weight#automatically creates getter and setter methods for us
 
@@ -26,8 +27,49 @@ sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
 puts sparky.info      # => Sparky weighs 10 lbs and is 12 inches tall.
 
 sparky.change_info('Spartacus', '24 inches', '45 lbs')
-puts sparky.info      # => Spartacus weighs 45 lbs and is 24 inches tall.       s
+puts sparky.info   # => Spartacus weighs 45 lbs and is 24 inches tall.       s
+
+#------------ MyCar Class ----------#
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    self.current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    self.current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{self.current_speed} mph."
+  end
+
+  def shut_down
+    self.current_speed = 0
+    puts "Let's park"
+  end
+
+  def spray_paint(color)
+     self.color = color
+     puts "Your new color is #{color}"
+  end
 
 
+end
 
+super_car = MyCar.new(2018, "white" , "perfect")
+puts super_car.color
+puts super_car.year
+super_car.spray_paint("blue")
 
