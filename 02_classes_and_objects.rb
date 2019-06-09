d4f8c161f7b57db6722ac732e6c2d9ebd2e0e028
gizemocak <gizem_ocak@outlook.com>
2019-06-09 21:36:48 +0000

    calling methods with self

--- 02_classes_and_objects.rb
+++ 02_classes_and_objects.rb
@@ -1,34 +1,32 @@
 class GoodDog
-  attr_accessor :name #automatically creates getter and setter methods for us
+  attr_accessor :name, :height, :weight#automatically creates getter and setter methods for us
 
-  def initialize(name)
-    @name = name #@name is an instance variable
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
   end
-  
-  # def name #renamed from get_name
-  #   @name
-  # end  
-
-  # def name=(name) #renamed from set_name
-  #   @name = name 
-  # end
 
   def speak
-    "#{@name} says arf!"
+    "#{name} says arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info 
+    "#{self.name} weights #{self.weight} and is #{self.height} tall."
   end
 end
 
-sparky = GoodDog.new("Sparky") #this obj was initialized
-puts sparky.speak
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info      # => Sparky weighs 10 lbs and is 12 inches tall.
 
-fido = GoodDog.new("Fido")
-puts fido.speak
-
-#puts sparky.name would return a NoMethodError , we would have to create a method that will return the name
-puts sparky.get_name
-sparky.set_name = "Sparkly"
-puts sparky.get_name
-
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info      # => Spartacus weighs 45 lbs and is 24 inches tall.       s
 
 
 
