eb6ab8df1e09d73c1acd96ae4f26a6fd1a522b64
gizemocak <gizem_ocak@outlook.com>
2019-06-10 02:41:48 +0000

    namespacing

---
+++ 09_more_modules.rb
@@ -0,0 +1,29 @@
+module Mammal
+  class Dog
+    def speak(sound)
+      p "#{sound}"
+    end
+  end
+
+  class Cat
+    def say_name(name)
+      p "#{name}"
+    end
+  end
+
+  def self.some_out_of_place_method(num)
+    num ** 2
+  end
+end
+
+#We call classes in a module by appending the class name to the module name with two colons(::)
+buddy = Mammal::Dog.new
+kitty = Mammal::Cat.new
+buddy.speak('Arf!')   
+kitty.say_name('kitty')
+
+#using modules as a container
+value = Mammal.some_out_of_place_method(4)
+value = Mammal::some_out_of_place_method(4)
+
+
